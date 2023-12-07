2a7de2041eb7fa9d546b6816c2fe08fca00245c0
omursahin <omur@erciyes.edu.tr>
2023-12-07 19:54:29 +0300

    fix

--- apps/design_patterns/main.py
+++ apps/design_patterns/main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('https://www.youtube.com/watch?v=Opxhh9Oh3rg')
+    print('https://open.spotify.com/intl-tr/track/4vziJcnB2Qyi9o4nIRUeN7?si=4a45ec3d63b94707')
